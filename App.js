20a538c64b2b3a3e5933298ca146a26f7affe8bf
VARSHA RANA <159460240+ranavarsha@users.noreply.github.com>
2025-07-08 23:10:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>QuickCart</h1>
+      <p>Welcome to the smart e-commerce platform.</p>
+    </div>
+  );
+}
+
+export default App;
